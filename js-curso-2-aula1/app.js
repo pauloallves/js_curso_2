3e0845d72afdb91ff5223307f8314e6e8150981f
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-11 20:25:03 -0300

    testando funções com parâmetros

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -1,8 +1,19 @@
-let titulo = document.querySelector('h1');
+/*let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';*/
+
+
+function exibirTextoNaTela(tag, texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Bem-vindo ao Jogo do número secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+
+
 
 function verificarChute(){
     console.log('Botão foi clicado.')
