da635bff832011bd425ecf0d9022e27696ce8407
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-12 15:08:31 -0300

    verificado o chute

--- js-curso-2-aula_2/app.js
+++ js-curso-2-aula_2/app.js
@@ -1,4 +1,4 @@
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 5 //gerarNumeroAleatorio();
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -10,7 +10,17 @@ exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    console.log(chute == numeroSecreto);
+    //console.log(chute == numeroSecreto);
+    if (chute == numeroSecreto){
+        exibirTextoNaTela('h1', 'Você acertou!');
+        exibirTextoNaTela('p', 'Parabéns, você descobriu o número secreto.');
+    }else {
+        if(chute > numeroSecreto){
+            exibirTextoNaTela('p', 'O chute é maior que o número secreto.');
+        }else{
+            exibirTextoNaTela('p', 'O chute é menor que o número secreto.');
+        }
+    }
 }
 
 function gerarNumeroAleatorio() {
