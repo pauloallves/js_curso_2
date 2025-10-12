f2e1ef82246c5d239b0ab3b3010f3ec126654431
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-11 21:18:31 -0300

    conhecendo o tipo booleano em js

--- js-curso-2-aula1/app.js
+++ js-curso-2-aula1/app.js
@@ -3,7 +3,7 @@ titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';*/
-
+let numeroSecreto = gerarNumeroSecreto();
 
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
@@ -11,10 +11,15 @@ function exibirTextoNaTela(tag, texto){
 }
 
 exibirTextoNaTela('h1', 'Bem-vindo ao Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
-
-
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute(){
-    console.log('Botão foi clicado.')
+    let chute = document.querySelector('input').value;
+    console.log(numeroSecreto);
+    console.log(chute == numeroSecreto);
 }
+
+function gerarNumeroSecreto(){
+    return parseInt(Math.random() * 10 + 1);
+}
+
