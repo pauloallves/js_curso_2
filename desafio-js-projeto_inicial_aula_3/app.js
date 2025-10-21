0d4c10ac941adb7023887b2c3d7a742a80a47f55
Paulo Henrique <paulohenriquesantos280@gmail.com>
2025-10-21 00:34:08 -0300

    finalizando desafio da aula 3 e subindo repositorio da aula 4

---
+++ desafio-js-projeto_inicial_aula_3/app.js
@@ -0,0 +1,16 @@
+let listaGenerica = [];
+//------------------------------------------------------------
+let linguaguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+//------------------------------------------------------------
+linguaguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+console.log(linguaguagensDeProgramacao);
+//------------------------------------------------------------
+let listaDeNomes = ['Paulo', 'Alessandra', 'Alice'];
+console.log(listaDeNomes[0]);
+//------------------------------------------------------------
+let listaDePaises = ['Brasil', 'Alemanha', 'EUA'];
+console.log(listaDePaises[1]);
+//------------------------------------------------------------
+let listaDeCidades = ['Maceió', 'Atalaia', 'União dos Palmares'];
+console.log(listaDeCidades.length - 1);
+console.log(listaDeCidades[listaDeCidades.length - 1]);
